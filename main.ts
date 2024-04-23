8c28dc36df2617da1ec4061e692e3005801f7e5a
Rakhshan174 <160410026+Rakhshan174@users.noreply.github.com>
2024-04-23 08:27:56 -0700

    Q 29 TS/JS for 45 assignment

---
+++ main.ts
@@ -0,0 +1,28 @@
+// Favourit Fruit: Make a array of your favourit fruit, and then write a senes of independence if statements that
+// check for certain fruit in your array.
+
+//(a) Make a array your three favourit fruit and call favourit_fruit
+//(b) Write five if statements.Each should check whether a certain kind of furit is in your array. 
+//If the fruit is in your array, the if block should print a statment. such as You really like bananas!
+
+let favourit_fruit : String[] = ["Mango", "Orange", "WaterMelon", "berry", "peach"]
+
+if(favourit_fruit.includes("Mango")){
+    console.log("Mango")
+}
+
+if(favourit_fruit.includes("orange")){
+    console.log("orange")
+}
+if(favourit_fruit.includes("WaterMelon")){
+    console.log("WaterMelon")
+}
+
+if(favourit_fruit.includes("berry")){
+    console.log("You really like banana")
+}
+
+if(favourit_fruit.includes("peach")){
+    console.log("You really like banana")
+}
+
